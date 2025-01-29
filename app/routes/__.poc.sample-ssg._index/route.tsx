ffc220d866d21d9f916524377be90c41d6d218b0
atman-33 <gpbjk0304@gmail.com>
2025-01-29 23:42:54 +0900

    feat: SSGのサンプルページを追加

---
+++ app/routes/__.poc.sample-ssg._index/route.tsx
@@ -0,0 +1,18 @@
+const PocSampleSsgPage = () => {
+  return (
+    <>
+      <div className="font-bold text-xl">Sample SSG Page</div>
+      <div>
+        <h1>About Me</h1>
+        <p>
+          Hello! My name is Atman. I am a software developer with a passion for
+          creating web applications. I have experience in various technologies
+          including React, Node.js, and TypeScript. In my free time, I enjoy
+          learning new programming languages.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default PocSampleSsgPage;
