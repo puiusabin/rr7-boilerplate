c343cac5d63c940b453ce76df90e242a72e8f498
atman-33 <gpbjk0304@gmail.com>
2025-01-30 23:09:34 +0900

    feat: トップページからサンプルページへのリンクを追加

--- app/routes/_app._index/route.tsx
+++ app/routes/_app._index/route.tsx
@@ -17,12 +17,20 @@ export default function AppPage() {
       <Welcome />
       <div className="flex justify-center">
         <div className="flex flex-col gap-4">
-          <Link to="/poc/sample-ssr">
-            <Button className="w-72">Go to Sample SSR (Pokemon List)</Button>
+          <Link to="/poc/sample-csr" className="flex justify-center">
+            <Button className="w-80">Go to Sample CSR (Pokemon List)</Button>
           </Link>
-          <Link to="/poc/sample-ssg">
-            <Button className="w-72">Go to Sample SSG (About Me)</Button>
-          </Link>{' '}
+          <Link to="/poc/sample-ssr" className="flex justify-center">
+            <Button className="w-80">Go to Sample SSR (Pokemon List)</Button>
+          </Link>
+          <Link to="/poc/sample-ssg" className="flex justify-center">
+            <Button className="w-80">Go to Sample SSG (About Me)</Button>
+          </Link>
+          <Link to="/poc/sample-ssr-csr" className="flex justify-center">
+            <Button className="w-80">
+              Go to Sample SSR & CSR (Pokemon List)
+            </Button>
+          </Link>
         </div>
       </div>
     </>
